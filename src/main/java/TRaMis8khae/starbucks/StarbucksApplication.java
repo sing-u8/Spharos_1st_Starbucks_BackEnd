9a8b861154aaedf2ac0dc98cfef83c6f83979b07
Baek Seung Yeop <tmdduq1999@naver.com>
2024-08-27 14:10:55 +0900

    [TEST]: Test code

---
+++ src/main/java/TRaMis8khae/starbucks/StarbucksApplication.java
@@ -0,0 +1,13 @@
+package TRaMis8khae.starbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TRaMis8khae/starbucks/StarbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package TRaMis8khae.starbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
