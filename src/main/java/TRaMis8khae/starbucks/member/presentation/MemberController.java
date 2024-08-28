d8c8f5b83f72198e6a76d4e6e801d819935cf112
tlswl7479 <tlswl7479@gmail.com>
2024-08-28 14:38:58 +0900

    Add Presentation

---
+++ src/main/java/TRaMis8khae/starbucks/member/presentation/MemberController.java
@@ -0,0 +1,9 @@
+package TRaMis8khae.starbucks.member.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member") // 임시
+public class MemberController {
+}
