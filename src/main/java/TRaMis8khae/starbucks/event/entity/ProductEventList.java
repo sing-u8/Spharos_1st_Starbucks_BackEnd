9688fa2a76acbd85abfdae2eda0620a5b83c2548
tlswl7479 <tlswl7479@gmail.com>
2024-09-13 13:50:21 +0900

    [TEST] : test

---
+++ src/main/java/TRaMis8khae/starbucks/event/entity/ProductEventList.java
@@ -0,0 +1,18 @@
+package TRaMis8khae.starbucks.event.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ProductEventList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Event eventId;
+
+}
