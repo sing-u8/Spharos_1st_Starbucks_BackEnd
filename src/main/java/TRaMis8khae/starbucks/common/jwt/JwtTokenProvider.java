cac1a72d2619e130c4d73be49140c4f387f4c128
tlswl7479 <tlswl7479@gmail.com>
2024-09-14 01:13:47 +0900

    [FIX] : auth 관련 기능 수정 중

--- src/main/java/TRaMis8khae/starbucks/common/jwt/JwtTokenProvider.java
+++ src/main/java/TRaMis8khae/starbucks/common/jwt/JwtTokenProvider.java
@@ -1,5 +1,7 @@
 package TRaMis8khae.starbucks.common.jwt;
 
+import TRaMis8khae.starbucks.common.entity.BaseResponseStatus;
+import TRaMis8khae.starbucks.common.exception.BaseException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
@@ -30,7 +32,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+//    private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
     private final Environment env;
     private final UserDetailsService userDetailsService;
 
@@ -40,31 +42,12 @@ public class JwtTokenProvider {
         Date now = new Date();
         Date expiration = new Date(now.getTime() + env.getProperty("jwt.access-expire-time", Long.class).longValue());
 
-         String token = Jwts.builder()
+         return Jwts.builder()
                 .signWith(getSignKey())
                 .claim("memberUUID", claims.getSubject())
                 .setExpiration(expiration)
                 .compact();
 
-        log.info("Generated JWT AccessToken: {}", token);
-        log.info("Login ID: {}", claims.getSubject());
-
-         return token;
-
-        //        Claims claims = Jwts.claims().subject(authentication.getName()).build();
-//        Date now = new Date();
-//        Date expiration = new Date(now.getTime() + env.getProperty("jwt.access-expire-time", Long.class).longValue());
-//
-//        String token = Jwts.builder()
-//                .signWith(getSignKey())
-//                .claim("memberUuid", claims.getSubject())
-//                .issuedAt(expiration)
-//                .compact();
-//
-//        logger.info("Generated JWT AccessToken: {}", token);
-//        logger.info("Login ID: {}", claims.getSubject());
-//
-//        return token;
     }
 
     public String generateRefreshToken(Authentication authentication) {
@@ -76,7 +59,7 @@ public class JwtTokenProvider {
         return Jwts.builder()
                 .signWith(getSignKey())
                 .claim("email", claims.getSubject())
-                .setIssuedAt(expiration)
+                .issuedAt(expiration)
                 .compact();
     }
 
@@ -84,6 +67,7 @@ public class JwtTokenProvider {
         return Keys.hmacShaKeyFor( env.getProperty("jwt.secret-key").getBytes() );
     }
 
+    // 토큰에서 모든 클레임 추출
     public Claims getClaims(String token) {
         return Jwts.parser()
                 .setSigningKey(getSignKey())
@@ -92,24 +76,32 @@ public class JwtTokenProvider {
                 .getBody();
     }
 
-    public Authentication getAuthentication(String token) {
-
-        log.info("token : {}", token);
-
-        Claims claims = getClaims(token);
-        log.info("claims : {}", claims);
-
-        String memberUUID = claims.get("memberUUID", String.class);
-
-        log.info("memberUUID : {}", memberUUID);
-
-        UserDetails userDetails = userDetailsService.loadUserByUsername(memberUUID);
-
-        return new UsernamePasswordAuthenticationToken(
-                userDetails,
-                null,
-                userDetails.getAuthorities()
-        );
+    public String getMemberUUID(String token) throws BaseException {
+        try {
+            log.info(extractClaim(token, Claims::getSubject));
+        } catch (NullPointerException e) {
+            throw new BaseException(BaseResponseStatus.WRONG_JWT_TOKEN);
+        }
     }
+
+//    public Authentication getAuthentication(String token) {
+//
+//        log.info("token : {}", token);
+//
+//        Claims claims = getClaims(token);
+//        log.info("claims : {}", claims);
+//
+//        String memberUUID = claims.get("memberUUID", String.class);
+//
+//        log.info("memberUUID : {}", memberUUID);
+//
+//        UserDetails userDetails = userDetailsService.loadUserByUsername(memberUUID);
+//
+//        return new UsernamePasswordAuthenticationToken(
+//                userDetails,
+//                null,
+//                userDetails.getAuthorities()
+//        );
+//    }
 
 }
